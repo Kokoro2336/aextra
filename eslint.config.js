06e51cdfd4a773666e4dcbb370d72e6dd276e875
longlin10086 <gnulonglin@gmail.com>
2025-06-29 16:31:09 +0800

    chores: remove react from eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,25 +1,23 @@
-import js from '@eslint/js'
-import eslintPluginAstro from 'eslint-plugin-astro'
-import pluginReact from 'eslint-plugin-react'
-import { defineConfig } from 'eslint/config'
-import globals from 'globals'
-import tseslint from 'typescript-eslint'
+import js from "@eslint/js";
+import eslintPluginAstro from "eslint-plugin-astro";
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
 export default defineConfig([
   {
-    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
     plugins: { js },
-    extends: ['js/recommended']
+    extends: ["js/recommended"],
   },
   {
-    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-    languageOptions: { globals: globals.browser }
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    languageOptions: { globals: globals.browser },
   },
   tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
   ...eslintPluginAstro.configs.recommended,
   // Ignore files
   {
-    ignores: ['public/scripts/*', 'scripts/*', '.astro/', 'src/env.d.ts']
-  }
-])
+    ignores: ["public/scripts/*", "scripts/*", ".astro/", "src/env.d.ts"],
+  },
+]);
