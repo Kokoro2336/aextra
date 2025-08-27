141624b5ca909e9ec91369c502602580d24aecbb
syhanjin <syhanjin666@gmail.com>
2025-08-27 16:13:01 +0800

    chore(deps): add React hooks check

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,36 @@
-import js from '@eslint/js'
-import eslintPluginAstro from 'eslint-plugin-astro'
-import { defineConfig } from 'eslint/config'
-import globals from 'globals'
-import tseslint from 'typescript-eslint'
+import js from "@eslint/js";
+import eslintPluginAstro from "eslint-plugin-astro";
+import pluginReactHooks from "eslint-plugin-react-hooks";
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
 export default defineConfig([
   {
-    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
     plugins: { js },
-    extends: ['js/recommended']
+    extends: ["js/recommended"],
   },
   {
-    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-    languageOptions: { globals: globals.browser }
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    languageOptions: { globals: globals.browser },
   },
   tseslint.configs.recommended,
   ...eslintPluginAstro.configs.recommended,
   // Ignore files
   {
-    ignores: ['public/scripts/*', 'scripts/*', '.astro/', 'src/env.d.ts']
-  }
-])
+    ignores: ["public/scripts/*", "scripts/*", ".astro/", "src/env.d.ts"],
+  },
+  {
+    plugins: {
+      "react-hooks": pluginReactHooks,
+    },
+    rules: {
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+      "no-unused-vars": "warn",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "react/react-in-jsx-scope": "off",
+    },
+  },
+]);
